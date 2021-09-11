cda5929f97f1755e458ea268e49172f299874dbf
pedrogamedev <90098120+pedrogamedev@users.noreply.github.com>
2021-09-11 11:28:24 -0300

    script 3 branch 3

--- Assets/script/script3.cs
+++ Assets/script/script3.cs
@@ -7,7 +7,7 @@ public class script3 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        var x = 0;
     }
 
     // Update is called once per frame
