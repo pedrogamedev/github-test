41dbfb968cbaa3d5409c18e9c576edb8f12a86fe
pedrogamedev <90098120+pedrogamedev@users.noreply.github.com>
2021-09-11 10:46:07 -0300

    comit teste 3

--- Assets/script/scripts.cs
+++ Assets/script/scripts.cs
@@ -7,7 +7,8 @@ public class scripts : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        var x = 0;
+
     }
 
     // Update is called once per frame
